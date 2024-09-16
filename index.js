24261e48e7990a77d6ec34746a3286506cfe3fe0
Salomé <salome.2001.dias@gmail.com>
2024-09-16 19:22:11 +0100

    changes on 16/09/2024

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/login.ejs", (req,res) =>{
+    res.render("login.ejs");
+});
+
+app.get("/perfil.ejs", (req, res) => {
+    res.render("perfil.ejs");
+});
+
+app.post("/artigo/:id", (req, res) => {
+    res.render("artigo.ejs");
+});
+
+app.get("/compra.ejs", (req, res) => {
+    res.render("compra.ejs");
+})
+
+/*app.post("index.ejs", (req, res) => {
+
+
+    res.render(index.ejs)
+});*/
+
+app.listen(port, () => {
+    console.log(`Successfully started server on port ${port}.`);
+});
