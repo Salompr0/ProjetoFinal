07d058bf055753bb9a0ac7de93569e7fc11c77a3
Mariana Vidal <marianapompermaier@hotmail.com>
2024-10-07 17:30:09 +0100

    Update das paginas

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
+import express from "express";
 import { dirname, join } from "path";
+import pg from "pg";
 import { fileURLToPath } from "url";
+//import bcrypt from "bcrypt";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //The path names
@@ -10,10 +12,10 @@ const home = join(__dirname, "views/index.ejs");
 const login = join(__dirname, "views/login.ejs");
 const registo = join(__dirname, "views/registar.ejs");
 const perfil = join(__dirname, "views/perfil.ejs");
-const artistas = join(__dirname, "views/artistas.ejs");
-const artigo = join(__dirname, "views/artigo.ejs");
+const artigoescolhido = join(__dirname, "views/artigo.ejs");
 const categorias = join(__dirname, "views/categorias.ejs");
 const compra = join(__dirname, "views/compra.ejs");
+const registoArt = join(__dirname, "views/registarArtigo.ejs");
 
 const app = express();
 const port = 3000;
@@ -32,55 +34,182 @@ const db = new pg.Client({
 //CSS Path
 app.use(express.static("public"));
 
+//app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render(home);
-});
+//Função para obter categorias
+async function getCategorias(){
+    let categoria = [];
+    
+    const result = await db.query("SELECT * FROM categoria");
 
-app.get("/login", (req,res) =>{
-    res.render(login);
+
+    categoria = result.rows;
+    
+    //console.log(categoria);
+
+    return categoria;
+}
+
+//Função para obter artigos de uma categoria
+async function getArtigos(){
+
+    let artigos = [];
+
+    const result = await db.query("SELECT * FROM artigo ORDER BY art_id ASC");
+
+    artigos = result.rows;
+
+    //console.log(artigos);
+    
+    return artigos;
+}
+
+//Página principal
+app.get("/", async (req, res) => {
+    //Categorias em Destaque (Acrílico, Aguarelas)
+    const categoriaDestaque = await getCategorias();
+
+    const artigos = await getArtigos();
+
+    let idArtigo = [];
+
+    let newRow = [];
+
+    res.render(home, { categoria: categoriaDestaque, artigo: artigos, totalArtigo: artigos.length, idArtigo: idArtigo, newRow: newRow});
 });
 
 app.get("/registar", (req, res) => {
     res.render(registo);
 });
 
-app.get("/perfil", (req, res) => {
-    res.render(perfil);
+//Página de Registo
+app.post("/registar", async (req, res) => {
+
+    const nome = req.body["nome"];
+    const email = req.body["email"];
+    const telemovel = req.body["telemovel"];
+    const nif = req.body["nif"];
+    const morada = req.body["morada"];
+    const qualificacao = req.body["qualificacao"];
+    const vendedor = req.body["vendedor"];
+    const img = req.body["img"];
+    const password = req.body["password"];
+    
+    try{
+        const checkResult = await db.query("SELECT FROM users WHERE email = $1", [email]);
+
+        if (checkResult.rows.length > 0){
+            res.send("Esse email já existe. Tente fazer login.");
+        } else {
+            const result = await db.query("INSERT INTO users (user_nome, email, telemovel, nif, morada, qualificacao, vendedor, img_user, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)", [nome, email, telemovel, nif, morada, qualificacao, vendedor, img, password]);
+
+            console.log(result);
+            res.render(home);
+        }
+    } catch (err){
+        console.log(err);
+    }
 });
 
+app.get("/login", (req, res) => {
+    res.render(login);
+});
 
+//Página de login
+app.post("/login", async (req,res) =>{
+    
+    //const loggedin = false;
+    const email = req.body["nome"];
+    const password = req.body["password"];
+
+    try {
+        const result = await db.query("SELECT email, password FROM users WHERE email = $1", [email]);
+
+        if (result.rows > 0){
+            const utilizador = result.rows[0];
+            const pass = utilizador.password;
+            
+            if (password === pass){
+
+                //loggedin = true;
+
+                res.render(home);
+            } else {
+                res.send("Password Incorreta");
+            }
+        } else {
+            res.send("Utilizador não encontrado");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+//Help with loggin: https://codeshack.io/basic-login-system-nodejs-express-mysql/
+});
+
+//Página de Perfil do Utilizador
+app.get("/perfil/:id", async (req, res) => {
+    const userID = parseInt(req.params.id);
+
+    const result = await db.query('SELECT * FROM user WHERE user_id = $1', [userID]);
+    const perfil = result.rows;
+
+    //console.log(userID);
+    console.log(perfil);
+
+    res.render(perfil, { perfil: perfil});
+});
+
+app.patch("edit/user/:id", (req, res) => {
+
+    
+    res.render(registo);
+});
+
+//Página com todas as categorias
 app.get("/categorias", async (req, res) => {
 
-    let categoria = [];
+    const categoria = await getCategorias();
+    const artigo = await getArtigos();
     
-    const result = await db.query("SELECT cat_nome FROM categoria");
+    res.render(categorias, { categoria: categoria, total: categoria.length, artigo: artigo, totalArtigo: artigo.length});
 
-    result.rows.forEach((cat) => {
-        categoria.push(cat.cat_nome);
-    });
-    
-    console.log(result.rows);        
-
-    res.render(categorias, { categoria: categoria, total: categoria.length});
-    db.end();
 });
 
+//Registo de artigo
+
+app.get("/registoArtigo", (req, res) => {
+    res.render(registoArt);
+})
+
+app.post("/registoArtigo", async (req, res) => {
+
+    const categorias = await getCategorias();
+
+    res.render(registoArt, {categorias: categorias, total: categorias.length});
+});
+
+//Página de um artigo
+app.get("/arte/:id", async (req, res) => {
+
+    const artID = parseInt(req.params.id);
+
+    const result = await db.query('SELECT * FROM artigo WHERE art_id = $1', [artID]);
+    const artigo = result.rows[0];
+
+    //console.log(artigo);
+
+    res.render(artigoescolhido, { artigos: artigo});
+});
+
+//Página do carrinho de compras
 app.get("/compra", (req, res) => {
-    res.render(compra);
+
+    const pedidos = 0;//change
+
+    res.render(compra, { pedidos: pedidos});
 });
-
-
-
-
-app.post("/artigo/:id", (req, res) => {
-
-    res.render(artigo);
-});
-
-
-
 
 
 
