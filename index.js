2923c6ecb165769bf1506a8eaa498f976f3d3341
Salomé <salome.2001.dias@gmail.com>
2024-10-12 13:39:11 +0100

    Update artigos

--- index.js
+++ index.js
@@ -157,10 +157,12 @@ app.get("/perfil", async (req, res) => {
 //Página com todas as categorias
 app.get("/categorias", async (req, res) => {
 
+    const loggedin = req.isAuthenticated();
+
     const categoria = await getCategorias();
     const artigo = await getArtigos();
     
-    res.render(categorias, { categoria: categoria, total: categoria.length, artigo: artigo, totalArtigo: artigo.length});
+    res.render(categorias, { categoria: categoria, total: categoria.length, artigos: artigo, loggedin: loggedin });
 
 });
 
@@ -196,6 +198,14 @@ app.get("/editarArtigo/:id", async (req, res) => {
 app.get("/arte/:id", async (req, res) => {
 
     const loggedin = req.isAuthenticated();
+
+    const userID = req.user.user_id;;
+
+    if(req.isAuthenticated() === false){
+        
+        userID = 0;
+    }
+
     const artID = parseInt(req.params.id);
     const users = await getUsers();
 
@@ -204,7 +214,7 @@ app.get("/arte/:id", async (req, res) => {
 
     //console.log(artigo);
 
-    res.render(artigoescolhido, { artigos: artigo, loggedin: loggedin, users: users, totalUsers: users.length });
+    res.render(artigoescolhido, { artigos: artigo, loggedin: loggedin, users: users, totalUsers: users.length, artista: userID });
 });
 
 app.post("/editarArtigo/:id", async (req, res) => {
